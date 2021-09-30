1350c09a92ec68c5c20ded44e55ebe4f2cd14472
Cilxyxz <91652092+Cilxyxz@users.noreply.github.com>
2021-09-30 01:51:34 -0400

    Update ipinfo.py

--- ipinfo.py
+++ ipinfo.py
@@ -16,60 +16,67 @@ def slowprint(s):
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(1. / 1000)
-        
-        def ipinfo():
+
+
+def ipinfo():
        ip=input(" Enter IP Address : \033[1;32m ")
        url = ("http://ip-api.com/json/")
        response = urllib.request.urlopen(url + ip)
        data = response.read()
        values = json.loads(data)
        os.system("clear")
-        
-       print ("\033[0;205m\007\n")
+
+       print ("\033[1;32m\007\n")
        os.system("figlet IP-Info | lolcat")
-       slowprint("\033[0;205 ════════════════════════════════════════════════════════")
-       slowprint("\033[0;205" + "\n IP          : \033[1;32m " + values['query'])
-       slowprint("\033[0;205" + " Status      : \033[0;205 " + values['status'])
-       slowprint("\033[0;205" + " Region      : \033[0;205 " + values['regionName'])
-       slowprint("\033[0;205" + " Country     : \033[0;205 " + values['country'])
-       slowprint("\033[0;205" + " Date & Time : \033[0;205 " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-       slowprint("\033[0;205" + " City        : \033[0;205 " + values['city'])
-       slowprint("\033[0;205" + " ISP         : \033[0;205 " + values['isp'])
-       slowprint("\033[0;205" + " Lat,Lon     : \033[0;205 " + str(values['lat']) + "," + str(values['lon']))
-       slowprint("\033[0;205" + " ZIPCODE     : \033[0;205 " + values['zip'])
-       slowprint("\033[0;205" + " TimeZone    : \033[0;205 " + values['timezone'])
-       slowprint("\033[0;205" + " AS          : \033[0;205 " + values['as'] + "\n")
-       slowprint("\033[0;205 ════════════════════════════════════════════════════════") 
+       slowprint("\033[1;36m =====================================")
+       slowprint("\033[1;33m|            IP Information           |")
+       slowprint("\033[1;36m =====================================")
+       slowprint("\033[1;36m" + "\n IP          : \033[1;32m " + values['query'])
+       slowprint("\033[1;36m" + " Status      : \033[1;32m " + values['status'])
+       slowprint("\033[1;36m" + " Region      : \033[1;32m " + values['regionName'])
+       slowprint("\033[1;36m" + " Country     : \033[1;32m " + values['country'])
+       slowprint("\033[1;36m" + " Date & Time : \033[1;32m " + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+       slowprint("\033[1;36m" + " City        : \033[1;32m " + values['city'])
+       slowprint("\033[1;36m" + " ISP         : \033[1;32m " + values['isp'])
+       slowprint("\033[1;36m" + " Lat,Lon     : \033[1;32m " + str(values['lat']) + "," + str(values['lon']))
+       slowprint("\033[1;36m" + " ZIPCODE     : \033[1;32m " + values['zip'])
+       slowprint("\033[1;36m" + " TimeZone    : \033[1;32m " + values['timezone'])
+       slowprint("\033[1;36m" + " AS          : \033[1;32m " + values['as'] + "\n")
        print (" ")
-       slowprint("\033[0;205        ══════════════════════════════════════")
-       slowprint("\033[0;205                Coded By Pwnicated            ")
-       slowprint("\033[0;205        ══════════════════════════════════════")
+       slowprint("\033[1;36m =====================================")
+       slowprint("\033[1;33m|        Coded By VPPHacker           |")
+       slowprint("\033[1;36m =====================================")
+       slowprint("\033[1;91m|          www.vpphacker.com           |")
+       slowprint("\033[1;36m =====================================")
        print (" ")
-       slowprint("\033[0;205Press\033[0;205 ENTER\033[0;205 To Continue")
+       slowprint("\033[1;91mPress\033[1;36m ENTER\033[1;91m To Continue")
        print (" ")
-       vpp = input("\033[0;205m[+] IPInformation >>\033[0;2052m")
+       vpp = input("\033[1;33m[+] IPInformation >>\033[1;32m")
        if vpp == " ":
                 os.system("clear")
                 return main()
-                else:
-                os.system("clear")
-                return main()
-              def about():
+
+       else:
+            os.system("clear")
+            return main()
+
+
+def about():
        os.system("clear")
        print ("\033[1;32m\007\n")
        os.system("figlet IP-Info | lolcat")
        time.sleep(2)
-        
-       slowprint ("\033[0;205 ════════════════════════════════════════════════════════")
-       slowprint ("\033[0;205" + "         [+] Tool Name     =>\033[0;205" + " IP-Info")
-       slowprint ("\033[0;205" + "         [+] Autor         =>\033[0;205" + " Pwnicated")
-       slowprint ("\033[0;205" + "         [+] Github        =>\033[0;205" + " Cilxxyz")
-       slowprint ("\033[0;205 ════════════════════════════════════════════════════════")
-       magas = input("\033[0;205m [+] Press Enter To Continue [+]")
+       slowprint ("\033[1;91m -----------------------------------------------")
+       slowprint ("\033[1;33m" + "         [+] Tool Name     =>\033[1;36m" + " IP-Info")
+       slowprint ("\033[1;33m" + "         [+] Autor         =>\033[1;36m" + " VPPHacker")
+       slowprint ("\033[1;33m" + "         [+] Latest Update =>\033[1;36m" + " 17/3/2019")
+       slowprint ("\033[1;33m" + "         [+] YouTube       =>\033[1;36m" + " VPPHacker")
+       slowprint ("\033[1;33m" + "         [+] Github        =>\033[1;36m" + " VPPHacker")
+       slowprint ("\033[1;91m -----------------------------------------------")
+       slowprint ("\033[1;95m" + "          [+] www.vpphacker.com [+]          ")
+       slowprint ("\033[1;91m -----------------------------------------------")
+       magas = input("\033[1;33m [+] Press Enter To Continue [+]")
        if magas == " ":
-           os.system("clear")
-           return main()          
-           if magas == " ":
            os.system("clear")
            return main()
 
